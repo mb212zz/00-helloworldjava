26cc408268eb8a1e481674ecaf3caa687d2420da
mbenjelloun <mbenjelloun@350028R-F115X06.lise.kreizenn.bzh>
2026-09-22 15:04:35 +0200

    1er commit : modif

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -14,7 +14,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("Hello World m!");
     }
     
 }
